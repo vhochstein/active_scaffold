f85356f483b425a1c9373893abfcb3ff8191ef19
vhochstein <v.hochstein@highstone.de>
2012-02-11 13:01:41 +0100

    use activesupport lazy load hooks

--- lib/active_scaffold_env.rb
+++ lib/active_scaffold_env.rb
@@ -1,14 +1,20 @@
 # TODO: clean up extensions. some could be organized for autoloading, and others could be removed entirely.
 Dir["#{File.dirname __FILE__}/active_scaffold/extensions/*.rb"].each { |file| require file }
+ActiveSupport.on_load(:action_controller) do
+  include ActiveScaffold
+  include RespondsToParent
+  include ActiveScaffold::Helpers::ControllerHelpers
+  class_eval {include ActiveRecordPermissions::ModelUserAccess::Controller}
+end
 
-ActionController::Base.send(:include, ActiveScaffold)
-ActionController::Base.send(:include, RespondsToParent)
-ActionController::Base.send(:include, ActiveScaffold::Helpers::ControllerHelpers)
-ActionView::Base.send(:include, ActiveScaffold::Helpers::ViewHelpers)
+ActiveSupport.on_load(:action_view) do
+  include ActiveScaffold::Helpers::ViewHelpers
+end
 
-ActionController::Base.class_eval {include ActiveRecordPermissions::ModelUserAccess::Controller}
-ActiveRecord::Base.class_eval     {include ActiveRecordPermissions::ModelUserAccess::Model}
-ActiveRecord::Base.class_eval     {include ActiveRecordPermissions::Permissions}
+ActiveSupport.on_load(:active_record) do
+  class_eval {include ActiveRecordPermissions::ModelUserAccess::Model}
+  class_eval {include ActiveRecordPermissions::Permissions}
+end
 
 I18n.load_path += Dir[File.join(File.dirname(__FILE__), 'active_scaffold', 'locale', '*.{rb,yml}')]
 #ActiveScaffold.js_framework = :jquery
