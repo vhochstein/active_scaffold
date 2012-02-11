12a6ac70570d23ecc83d167ba9f4c3ddcb8dfcb9
vhochstein <v.hochstein@highstone.de>
2012-02-11 15:05:11 +0100

    lazy load activescaffold extensions

--- lib/active_scaffold_env.rb
+++ lib/active_scaffold_env.rb
@@ -1,6 +1,8 @@
 # TODO: clean up extensions. some could be organized for autoloading, and others could be removed entirely.
-Dir["#{File.dirname __FILE__}/active_scaffold/extensions/*.rb"].each { |file| require file }
 ActiveSupport.on_load(:action_controller) do
+  ['array', 'localize', 'nil_id_in_url_params', 'paginator_extensions', 'routing_mapper'].each do |extension|
+    require "#{File.dirname __FILE__}/active_scaffold/extensions/#{extension}.rb"
+  end
   include ActiveScaffold
   include RespondsToParent
   include ActiveScaffold::Helpers::ControllerHelpers
@@ -8,10 +10,16 @@ ActiveSupport.on_load(:action_controller) do
 end
 
 ActiveSupport.on_load(:action_view) do
+  ['name_option_for_datetime', 'action_view_rendering', 'action_view_resolver', 'usa_state'].each do |extension|
+    require "#{File.dirname __FILE__}/active_scaffold/extensions/#{extension}.rb"
+  end
   include ActiveScaffold::Helpers::ViewHelpers
 end
 
 ActiveSupport.on_load(:active_record) do
+  ['active_association_reflection', 'active_record_offset', 'reverse_associations', 'to_label', 'unsaved_record'].each do |extension|
+    require "#{File.dirname __FILE__}/active_scaffold/extensions/#{extension}.rb"
+  end
   class_eval {include ActiveRecordPermissions::ModelUserAccess::Model}
   class_eval {include ActiveRecordPermissions::Permissions}
 end
