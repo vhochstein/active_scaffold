825284e47ca61135159696baedb3d00c46ee13f7
vhochstein <v.hochstein@highstone.de>
2013-01-26 12:59:12 +0100

    do not use jquery live method anymore cause it is deprecated

--- app/assets/javascripts/jquery/tiny_mce_bridge.js
+++ app/assets/javascripts/jquery/tiny_mce_bridge.js
@@ -1,11 +1,11 @@
-$('form.as_form, form.inplace_form').live('as:form_loaded', function(event) {
+$(document).on('as:form_loaded', 'form.as_form, form.inplace_form', function(event) {
     var as_form = $(this).closest("form");
     as_form.find('textarea.as_mceEditor').each(function(index, elem) {
       tinyMCE.execCommand('mceAddControl', false, $(elem).attr('id'));
     });
     return true;
   });
-$('form.as_form, form.inplace_form').live('as:form_submit', function(event) {
+$(document).on('as:form_submit', 'form.as_form, form.inplace_form', function(event) {
     var as_form = $(this).closest("form");
     if (as_form.has('textarea.as_mceEditor').length > 0) {
       tinyMCE.triggerSave();
@@ -13,7 +13,7 @@ $('form.as_form, form.inplace_form').live('as:form_submit', function(event) {
     return true;
   });
 
-$('form.as_form, form.inplace_form').live('as:form_unloaded', function(event) {
+$(document).on('as:form_unloaded', 'form.as_form, form.inplace_form', function(event) {
     var as_form = $(this).closest("form");
     as_form.find('textarea.as_mceEditor').each(function(index, elem) {
       tinyMCE.execCommand('mceRemoveControl', false, $(elem).attr('id'));
