1255baea8c3a49b551af75df13e87e1531bd79b5
vhochstein <v.hochstein@highstone.de>
2014-05-31 16:45:55 +0200

    delete should just return id of deleted object for json, xml and yaml

--- lib/active_scaffold/actions/delete.rb
+++ lib/active_scaffold/actions/delete.rb
@@ -31,15 +31,18 @@ module ActiveScaffold::Actions
     end
 
     def destroy_respond_to_xml
-      render :xml => successful? ? "" : response_object.to_xml(:only => active_scaffold_config.list.columns.names), :content_type => Mime::XML, :status => response_status
+      column_names = successful? ? [active_scaffold_config.model.primary_key] : error_object_attributes
+      render :xml => successful? ? "" : response_object.to_xml(:only => column_names), :content_type => Mime::XML, :status => response_status
     end
 
     def destroy_respond_to_json
-      render :text => successful? ? "" : response_object.to_json(:only => active_scaffold_config.list.columns.names), :content_type => Mime::JSON, :status => response_status
+      column_names = successful? ? [active_scaffold_config.model.primary_key] : error_object_attributes
+      render :text => successful? ? "" : response_object.to_json(:only => column_names), :content_type => Mime::JSON, :status => response_status
     end
 
     def destroy_respond_to_yaml
-      render :text => successful? ? "" : Hash.from_xml(response_object.to_xml(:only => active_scaffold_config.list.columns.names)).to_yaml, :content_type => Mime::YAML, :status => response_status
+      column_names = successful? ? [active_scaffold_config.model.primary_key] : error_object_attributes
+      render :text => successful? ? "" : Hash.from_xml(response_object.to_xml(:only => column_names)).to_yaml, :content_type => Mime::YAML, :status => response_status
     end
 
     def destroy_find_record
