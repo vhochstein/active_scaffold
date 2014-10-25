045cc62223b6039e4caef1f7c1d53730ea3e6b4e
vhochstein <v.hochstein@highstone.de>
2014-10-25 13:08:02 +0200

    Bugfix: bridge.js needs to be loaded before activescaffold.js to guarantee that ready function is called before activescaffold ones

--- app/assets/javascripts/jquery/tiny_mce_bridge.js
+++ app/assets/javascripts/jquery/tiny_mce_bridge.js
@@ -1,22 +1,24 @@
-$(document).on('as:form_loaded', 'form.as_form, form.inplace_form', function(event) {
-    var as_form = $(this).closest("form");
-    as_form.find('textarea.as_mceEditor').each(function(index, elem) {
-      tinyMCE.execCommand('mceAddControl', false, $(elem).attr('id'));
+$(document).ready(function() {
+  $(document).on('as:form_loaded', 'form.as_form, form.inplace_form', function(event) {
+      var as_form = $(this).closest("form");
+      as_form.find('textarea.as_mceEditor').each(function(index, elem) {
+        tinyMCE.execCommand('mceAddControl', false, $(elem).attr('id'));
+      });
+      return true;
     });
-    return true;
-  });
-$(document).on('as:form_submit', 'form.as_form, form.inplace_form', function(event) {
-    var as_form = $(this).closest("form");
-    if (as_form.has('textarea.as_mceEditor').length > 0) {
-      tinyMCE.triggerSave();
-    }
-    return true;
-  });
+  $(document).on('as:form_submit', 'form.as_form, form.inplace_form', function(event) {
+      var as_form = $(this).closest("form");
+      if (as_form.has('textarea.as_mceEditor').length > 0) {
+        tinyMCE.triggerSave();
+      }
+      return true;
+    });
 
-$(document).on('as:form_unloaded', 'form.as_form, form.inplace_form', function(event) {
-    var as_form = $(this).closest("form");
-    as_form.find('textarea.as_mceEditor').each(function(index, elem) {
-      tinyMCE.execCommand('mceRemoveControl', false, $(elem).attr('id'));
+  $(document).on('as:form_unloaded', 'form.as_form, form.inplace_form', function(event) {
+      var as_form = $(this).closest("form");
+      as_form.find('textarea.as_mceEditor').each(function(index, elem) {
+        tinyMCE.execCommand('mceRemoveControl', false, $(elem).attr('id'));
+      });
+      return true;
     });
-    return true;
-  });
+});
