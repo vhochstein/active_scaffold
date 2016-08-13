9671a4f71ad263213ae5f82aa1269187d1daab49
Volker Hochstein <volker.hochstein@telefonica.com>
2016-08-13 11:24:24 +0200

    add support for Rails 3.2.22

--- lib/active_scaffold/extensions/action_view_resolver.rb
+++ lib/active_scaffold/extensions/action_view_resolver.rb
@@ -2,8 +2,13 @@ module ActionView
   class ActiveScaffoldResolver < FileSystemResolver
     # standard resolvers have a base path to views and append a controller subdirectory
     # activescaffolds view path do not have a subdir, so just remove the prefix
-    def find_templates(name, prefix, partial, details)
-      super(name,'',partial, details)
+    # > rails 3.2.22 uses an additional fifth parameter
+    def find_templates(name, prefix, partial, details, outside_app_allowed = false)
+      if ::Rails::VERSION::MAJOR >= 4 || (::Rails::VERSION::MAJOR == 3 && ::Rails::VERSION::MINOR == 2 && ::Rails::VERSION::TINY >= 22)
+        super(name,'',partial, details, outside_app_allowed)
+      else
+        super(name,'',partial, details)
+      end
     end
   end
 end
